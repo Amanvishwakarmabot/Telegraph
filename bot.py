b0544ef0372cb2359c474eff174c0ac1974315ff
Amanvishwakarmabot <147085734+Amanvishwakarmabot@users.noreply.github.com>
2024-07-14 11:54:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from Script import script
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
-from Av import web_server
+from Av.server import web_server
 
 import asyncio
 from pyrogram import idle
