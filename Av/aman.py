224b21d6f1c63d03cbd3856a51d003c4fc6c64aa
Amanvishwakarmabot <147085734+Amanvishwakarmabot@users.noreply.github.com>
2024-07-14 10:28:08 +0530

    Create aman.py

---
+++ Av/aman.py
@@ -0,0 +1,25 @@
+
+import re
+import logging
+from pymongo import MongoClient
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from config import API_ID, API_HASH, ADMINS, DB_NAME
+from config import DB_URI
+
+async def restart_bots():
+    logging.info("Restarting all bots........")
+    bots = list(mongo_db.bots.find())
+    for bot in bots:
+        bot_token = bot['token']
+        try:
+            ai = Client(
+                f"{bot_token}", API_ID, API_HASH,
+                bot_token=bot_token,
+                plugins={"root": "clone_plugins"},
+            )
+            await ai.start()
+        except Exception as e:
+            logging.exception(f"Error while restarting bot with token {bot_token}: {e}")
